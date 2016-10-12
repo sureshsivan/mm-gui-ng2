b3734d57bcd48d6e5fbf33d821927c9d7a7c3c81
v8-suresh <suresh.sivanantham@gmail.com>
2016-10-12 19:46:47 -0400

    Resetting to start on serverless architecture

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app',
-    template: `
-        <appHeader></appHeader>
-    `
-})
-export class AppComponent { }
--- src/header/Header.ts
+++
@@ -1,8 +0,0 @@
-interface Header{
-    title: string,
-    isLoggedIn: boolean,
-    showAuth0: boolean,
-    iconUrlLarge: string,
-    iconUrlMedium: string,
-    iconUrlSmall: string
-}
