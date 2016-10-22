78830ccd73d445cc229f73ccd93e5557c33ce511
v8-suresh <suresh.sivanantham@gmail.com>
2016-10-22 00:13:53 -0400

    cloudformation updated with sweet and small script

--- deploy/scripts/aws/create_or_update_stack.sh
+++ deploy/scripts/aws/create_or_update_stack.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 STACK_NAME=MM-NG2-WEB-STACK
-DOMAIN_NAME="sivashub.com"
-SUBDOMAIN_PREFIX="mm.ng2"
-REGION_HOSTED_ZONE_ID="Z3AQBSTGFYJSTF"
-DOMAIN_HOSTED_ZONE_ID="Z1SDAQ7OTDOJDU"
+ROOT_DOMAIN_NAME="sivashub.com"
+APP_DOMAIN_NAME="mm.ng2.sivashub.com"
 # check for stack existance
 # TODO : Check based on all status codes
 #CREATE NEW STACK IF [CREATE_FAILED, DELETE_COMPLETE]
@@ -28,31 +26,26 @@ DOMAIN_HOSTED_ZONE_ID="Z1SDAQ7OTDOJDU"
 #  X UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS
 #  X UPDATE_ROLLBACK_COMPLETE
 STACK_ALIVE="$("$AWS_CLI" cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE | grep "$STACK_NAME")"
-
+echo $ROOT_DOMAIN_NAME
+echo $APP_DOMAIN_NAME
 if [ -z "$STACK_ALIVE" ]; then
-    echo "[INFO] ${MM-NG2-WEB-STACK} Stack was never created or dead - recreating the complete stack : CREATING NEW STACK" >& 2
+    echo "[INFO] $STACK_NAME Stack was never created or dead - recreating the complete stack : CREATING NEW STACK" >& 2
     $AWS_CLI cloudformation create-stack \
             --stack-name $STACK_NAME \
             --template-body file://./deploy/cf-template-web-stack.json \
             --capabilities CAPABILITY_IAM \
             --parameters \
-                ParameterKey=PRoute53HostedZone,ParameterValue=$DOMAIN_NAME   \
-                ParameterKey=PRoute53SubDomainPrefix,ParameterValue=$SUBDOMAIN_PREFIX   \
-                ParameterKey=PRoute53RegionHostedZoneId,ParameterValue=$REGION_HOSTED_ZONE_ID   \
-                ParameterKey=PRoute53DomainHostedZoneId,ParameterValue=$DOMAIN_HOSTED_ZONE_ID   \
-            --region $AWS_REGION
+                ParameterKey=ParamRootDomain,ParameterValue=$ROOT_DOMAIN_NAME   \
+                ParameterKey=ParamAppDomain,ParameterValue=$APP_DOMAIN_NAME
     echo "[INFO] STACK CREATION : Kicked Off"
 else
-    echo "[INFO] ${MM-NG2-WEB-STACK} Stack was already built and alive : UPDATING EXISTING STACK" >& 2
+    echo "[INFO] $STACK_NAME Stack was already built and alive : UPDATING EXISTING STACK" >& 2
     $AWS_CLI cloudformation update-stack \
             --stack-name $STACK_NAME \
             --template-body file://./deploy/cf-template-web-stack.json \
             --capabilities CAPABILITY_IAM \
             --parameters \
-                ParameterKey=PRoute53HostedZone,ParameterValue=$DOMAIN_NAME   \
-                ParameterKey=PRoute53SubDomainPrefix,ParameterValue=$SUBDOMAIN_PREFIX   \
-                ParameterKey=PRoute53RegionHostedZoneId,ParameterValue=$REGION_HOSTED_ZONE_ID   \
-                ParameterKey=PRoute53DomainHostedZoneId,ParameterValue=$DOMAIN_HOSTED_ZONE_ID   \
-            --region $AWS_REGION
+                ParameterKey=ParamRootDomain,ParameterValue=$ROOT_DOMAIN_NAME   \
+                ParameterKey=ParamAppDomain,ParameterValue=$APP_DOMAIN_NAME
     echo "[INFO] STACK UPDATE : Kicked Off"
 fi
