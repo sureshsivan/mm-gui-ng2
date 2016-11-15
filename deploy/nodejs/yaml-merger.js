7a6762cd78598f4ee159bccfa3adaef609862f0b
v8-suresh <suresh.sivanantham@gmail.com>
2016-11-14 23:49:42 -0500

    yaml base configs changed.

---
+++ deploy/nodejs/yaml-merger.js
@@ -0,0 +1,36 @@
+var JsonRefs = require('json-refs');
+var YAML = require('yaml-js');
+var fs = require('fs');
+var path = require('path');
+
+const OUTPUT_FILE_NAME = "template.yaml";
+const OUTPUT_FOLDER_NAME = process.cwd() + "/.build/";
+
+var mergeYaml = function(rootYaml){
+    var relativeBase = path.dirname(rootYaml);
+    var root = YAML.load(fs.readFileSync(rootYaml).toString());
+    console.log(root);
+    var options = {
+        filter: function(ref){
+            return ref.uri.indexOf('#/') !== 0;
+        },
+        includeInvalid: true,
+        loaderOptions : {
+            processContent: function (res, cb) {
+                cb(undefined, YAML.load(res.text));
+            }
+        },
+        relativeBase: relativeBase
+    };
+    JsonRefs.resolveRefs(root, options)
+        .then(function (res) {
+            console.log((OUTPUT_FOLDER_NAME + OUTPUT_FILE_NAME));
+            // fs.writeFileSync('./dist/swagger2.json', JSON.stringify(res.resolved, null, 2), 'utf8');
+            fs.writeFileSync((OUTPUT_FOLDER_NAME + OUTPUT_FILE_NAME),
+                YAML.dump(res.resolved),
+                'utf8');
+        }, function (err) {
+            console.error(err.stack);
+        });
+};
+mergeYaml("deploy/scripts/aws-cf/web-no-cdn.yaml");
